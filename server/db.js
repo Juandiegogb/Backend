a62eedba1e7335eaf2206a8b6715158792635885
Juan Diego Garzón Becerra <juandiegar@gmail.com>
2024-10-02 05:22:54 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnect = () => {
   const uri = process.env.MONGO_URI;
   mongoose
-    .connect(uri, { dbName: "agenda-contactos" })
+    .connect(uri, { dbName: "contactosApp" })
     .then(() => {
       console.log("DB is ready");
     })
